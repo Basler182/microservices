4473022e75f5eb0a4e04224f1c44e2a12f2a8ddf
Basler182 <showdialog@web.de>
2022-04-30 19:48:51 +0200

    feat spring profiles

--- notification/src/main/java/de/runtimeterror/notification/NotificationApplication.java
+++ notification/src/main/java/de/runtimeterror/notification/NotificationApplication.java
@@ -2,6 +2,8 @@ package de.runtimeterror.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -9,6 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 "de.runtimeterror.amqp",
         }
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
