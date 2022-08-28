fa1c264f2414b1cf5377de3af2894fe28f51d069
Basler182 <showdialog@web.de>
2022-08-28 19:42:45 +0200

    feat add liquibase and api doc to customer service

--- customer/src/main/java/de/runtimeterror/customer/CustomerController.java
+++ customer/src/main/java/de/runtimeterror/customer/CustomerController.java
@@ -1,5 +1,9 @@
 package de.runtimeterror.customer;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/customers")
 public record CustomerController(CustomerService customerService) {
 
+    @Operation(summary = "Register a new customer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Customer registered successful"),
+            @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content)})
     @PostMapping
-    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("new customer registration {}", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
     }
