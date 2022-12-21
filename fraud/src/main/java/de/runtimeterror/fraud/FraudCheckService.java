4aa4a7f7e4c8f8014b1c0f40c924a96da3270054
Basler182 <showdialog@web.de>
2022-12-21 21:27:09 +0100

    feat retry fraudulent customer service

--- fraud/src/main/java/de/runtimeterror/fraud/FraudCheckService.java
+++ fraud/src/main/java/de/runtimeterror/fraud/FraudCheckService.java
@@ -1,9 +1,14 @@
 package de.runtimeterror.fraud;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @Service
 public class FraudCheckService {
 
@@ -13,13 +18,23 @@ public class FraudCheckService {
         this.fraudCheckHistoryRepository = fraudCheckHistoryRepository;
     }
 
-    public boolean isFraudulentCustomer(Integer customerId){
+    @Retryable(value = RuntimeException.class,
+            maxAttempts = 2,
+            backoff = @Backoff(delay = 1000))
+    public boolean isFraudulentCustomer(Integer customerId) {
         fraudCheckHistoryRepository.save(
-                FraudCheckHistory.builder().isFraudster(false)
+                FraudCheckHistory.builder()
+                        .isFraudster(false)
                         .customerId(customerId)
                         .createdAt(LocalDateTime.now())
                         .build()
         );
+        return false;
+    }
+
+    @Recover
+    public boolean recoverFromFraudulentCustomer(RuntimeException e, Integer customerId) {
+        log.error("Error while checking fraud for customer {}", customerId, e);
         return false;
     }
 }
